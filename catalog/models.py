9556f056ba4c571fc24e77a3d634c19cda619f04
Andrey <pise4kan8@gmail.com>
2025-05-01 23:46:44 +0300

    готовое дз

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name_category = models.CharField(
+        max_length=150,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    descriptions = models.TextField(
+        verbose_name="Описание категории", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name_category
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    descriptions = models.TextField(
+        verbose_name="Описание продукта", help_text="Введите описание продукта"
+    )
+    image = models.ImageField(
+        upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
