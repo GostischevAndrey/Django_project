3f27673d529784974e5bc634bbc9e02aed7782ff
Andrey <pise4kan8@gmail.com>
2025-05-16 22:35:51 +0300

    homework 25

---
+++ blog/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+from .apps import BlogConfig
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path("post_list/create/", views.PostCreate.as_view(), name="post_create"),
+    path("post_list/list/", views.PostListView.as_view(), name="post_list"),
+    path(
+        "post_list/detail/<int:pk>", views.PostDetailView.as_view(), name="post_detail"
+    ),
+    path(
+        "post_list/update/<int:pk>", views.PostUpdateView.as_view(), name="post_update"
+    ),
+    path(
+        "post_list/delete/<int:pk>", views.PostDeleteView.as_view(), name="post_delete"
+    ),
+]
